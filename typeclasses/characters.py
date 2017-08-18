b84b4aa27272ccf5214ee82c3e685fec838595f3
streetwitch <adventmagic@gmail.com>
2017-08-18 20:47:45 +0000

    log exits

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -453,6 +453,7 @@ class Character(DefaultCharacter):
             string = msg
         elif self.db.alive == 1 and self.db.invis == 0 and not self.ndb.sneak:
             string = "{object} is leaving {origin}, heading for {destination}."
+            self.location.log_action(string)
         elif self.ndb.sneak:
             total = self.db.dexterity + self.db.stealth
             winz = 0
@@ -466,7 +467,7 @@ class Character(DefaultCharacter):
                 string = ""
         else:
             string =  "{object} is leaving {origin}, heading for {destination}."
-
+            self.location.log_action(string)
         location = self.location
         exits = [o for o in location.contents if o.location is location and o.destination is destination]
         if not mapping:
@@ -525,6 +526,7 @@ class Character(DefaultCharacter):
                 string = msg
             elif self.db.alive == 1 and self.db.invis == 0 and not self.ndb.sneak:
                 string = "{object} arrives to {destination} from {origin}."
+                self.location.log_action(string)
             elif self.ndb.sneak:
                 total = self.db.dexterity + self.db.stealth
                 winz = 0
@@ -540,6 +542,7 @@ class Character(DefaultCharacter):
                 string = "" 
         elif self.db.alive == 1 and self.db.invis ==0 and not self.ndb.sneak:
             string = "{object} arrives to {destination}."
+            self.location.log_action(string)
         elif self.ndb.sneak:
             total = self.db.dexterity + self.db.stealth
             winz = 0
