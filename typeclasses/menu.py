c36c29144d7c849b528346ee1e28153b5df1a530
streetwitch <adventmagic@gmail.com>
2017-08-25 19:06:30 +0000

    punch

--- typeclasses/menu.py
+++ typeclasses/menu.py
@@ -1187,6 +1187,14 @@ def dodge(caller):
             defendpoints = dmg2
             reduced = 0
 
+        cnt2 = 0
+        final = 0
+        while (cnt2 < reduced):
+            cnt2 = cnt2 + 1
+            roll = roll_dice(1, 10)
+            if (roll >= 6):
+                final = final + 1
+                
         if(caller.db.target.db.weapon == 0):
             caller.msg("|/|gYou dodge %i out of %i of %s's attack points." % (defendpoints, dmg2, caller.db.target))
             if(caller.db.target.db.form == "cat"):
@@ -1195,21 +1203,22 @@ def dodge(caller):
             if(caller.db.target.db.form == "dog"):
                 if reduced > 3:
                    reduced = 3
-            caller.msg("|/|g%s causes %i points of damage to you." % (caller.db.target, reduced))
-            if (soakpoints > reduced):
-                soakpoints = reduced
+            caller.msg("|/|g%s rolls %i dice of damage points to you." % (caller.db.target, reduced))
+            caller.msg("|/|g%s causes %i points of damage to you." % (caller.db.target, final))
+            if (soakpoints > final):
+                soakpoints = final
             if (soakpoints > 0):
-                caller.msg("|/|gYou soak %i out of %i points of bashing damage." % (soakpoints, reduced))
+                caller.msg("|/|gYou soak %i out of %i points of bashing damage." % (soakpoints, final))
             if (reduced - soakpoints > 0):
-                caller.msg("|/|gYou lose a total of %i health points." % (reduced - soakpoints))
-                caller.db.bashing = caller.db.bashing + (reduced - soakpoints)
+                caller.msg("|/|gYou lose a total of %i health points." % (final - soakpoints))
+                caller.db.bashing = caller.db.bashing + (final - soakpoints)
             caller.db.target.msg("|/|g%s dodges %i points of your attack." % (caller, defendpoints))
-            caller.db.target.msg("|/|gYou deal %i points of damage with your attack." % (reduced))
+            caller.db.target.msg("|/|gYou deal %i points of damage with your attack." % (final))
     
             if(soakpoints>0):
                 caller.db.target.msg("|/|g%s soaks %i points of damage from your attack." % (caller, soakpoints))
             if(reduced-soakpoints > 0):
-                caller.db.target.msg("|/|g%s loses a total of %i hit points." % (caller, reduced - soakpoints))
+                caller.db.target.msg("|/|g%s loses a total of %i hit points." % (caller, final - soakpoints))
 
         if(caller.db.target.db.weapon == 1):
             caller.msg("|/|gYou dodge %i out of %i of %s's attack points." % (defendpoints, dmg2, caller.db.target))
