f7fe0b98c6d6b28ffe3de968bc783378b694d057
streetwitch <adventmagic@gmail.com>
2017-08-26 05:54:11 +0000

    firearms2

--- typeclasses/menu.py
+++ typeclasses/menu.py
@@ -1181,7 +1181,7 @@ def dodge(caller):
                 dmg = dmg + 1
         cnt2 = 0
         bonusdmg = 0
-        white(cn2 < dmg2 - defendpoints - 1):
+        while(cn2 < dmg2 - defendpoints - 1):
             cnt2 += 1
             roll = roll_dice(1,10)
             if (roll >= 6):
